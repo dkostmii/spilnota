003e7a6877470e2ce62bcec128608cd9aac37691
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2023-10-06 13:45:28 +0300

    Fix title

--- app/src/components/screens/Home/Banner/index.tsx
+++ app/src/components/screens/Home/Banner/index.tsx
@@ -8,7 +8,7 @@ const Banner: FC = () => (
   <div className={style.body}>
     <div className={style.content}>
       <div className={style.heading}>
-        <h1 className={style.title}>«Спільнота - Рідні»</h1>
+        <h1 className={style.title}>Спільнота - Рідні</h1>
         <h2 className={style.subtitle}>Проєкт Благодійного фонду «Рідні»</h2>
       </div>
 
